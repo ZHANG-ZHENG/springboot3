3a02d082b9c002e6a11994bb43e12216119edb46
zhengzhang <zhengzhang@ruijie.com.cn>
2023-03-23 18:42:30 +0800

    gradle demo.

---
+++ boot-gradle-demo/src/main/java/com/example/test/BootGradleDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootGradleDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootGradleDemoApplication.class, args);
+	}
+
+}
