20ba2b96cec25d3cf26ee5bddb94ae756e070a3c
zhengzhang <zhengzhang@ruijie.com.cn>
2023-03-28 10:17:56 +0800

    actuator

--- feign/feign-client/src/main/java/com/zz/test/FeignClientApplication.java
+++ feign/feign-client/src/main/java/com/zz/test/FeignClientApplication.java
@@ -8,6 +8,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class FeignClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(FeignClientApplication.class, args);
-        System.out.println("应用已启动测试接口访问：http://localhost:7001/feign-client/test/test1");
+        System.out.println("应用已启动");
+        System.out.println("测试接口访问：http://localhost:7001/feign-client/test/test1");
+        System.out.println("actuator测试接口访问：http://localhost:7001/feign-client/actuator/health");
+        System.out.println("actuator测试接口访问：http://localhost:7001/feign-client/actuator/info");
+        System.out.println("actuator测试接口访问：http://localhost:7001/feign-client/actuator/beans");
     }
 }
