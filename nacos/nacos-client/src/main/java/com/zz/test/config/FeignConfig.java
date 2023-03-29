3c23d2c3a9bc02497e6028c6c3a86cd0f2713e1d
zhengzhang <zhengzhang@ruijie.com.cn>
2023-03-29 16:01:24 +0800

    LoadBalancer miss

--- nacos/nacos-client/src/main/java/com/zz/test/config/FeignConfig.java
+++ nacos/nacos-client/src/main/java/com/zz/test/config/FeignConfig.java
@@ -3,9 +3,19 @@ package com.zz.test.config;
 
 import java.util.concurrent.TimeUnit;
 
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.loadbalancer.core.RandomLoadBalancer;
+import org.springframework.cloud.loadbalancer.core.ReactorLoadBalancer;
+import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
+import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import com.alibaba.cloud.nacos.NacosDiscoveryProperties;
+import com.alibaba.cloud.nacos.loadbalancer.NacosLoadBalancer;
 
 @Configuration
 @EnableFeignClients("com.zz.test.**.feign.**")
@@ -23,5 +33,20 @@ public class FeignConfig {
 				.writeTimeout(feignWriteTimeout, TimeUnit.SECONDS)
 				.build();
 	}
+
+//    @Bean
+//    @LoadBalanced
+//    public ReactorLoadBalancer<ServiceInstance> reactorServiceInstanceLoadBalancer(Environment environment,
+//            LoadBalancerClientFactory loadBalancerClientFactory) {
+//		String name = environment.getProperty(LoadBalancerClientFactory.PROPERTY_NAME);
+//		return new RandomLoadBalancer(
+//		loadBalancerClientFactory.getLazyProvider(name, ServiceInstanceListSupplier.class), name);
+//	}
+//    public ReactorLoadBalancer<ServiceInstance> nacosServiceInstanceLoadBalancer(Environment environment,
+//            LoadBalancerClientFactory loadBalancerClientFactory,
+//            NacosDiscoveryProperties nacosDiscoveryProperties) {
+//		String name = environment.getProperty(LoadBalancerClientFactory.PROPERTY_NAME);
+//		return new NacosLoadBalancer(loadBalancerClientFactory.getLazyProvider(name, ServiceInstanceListSupplier.class), name,nacosDiscoveryProperties);
+//	}    
 	
 }
